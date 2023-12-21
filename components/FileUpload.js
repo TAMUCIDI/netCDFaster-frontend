6dad1d131549d1504e5e885f3b13c02a33ff9f7d
laosong-2020 <songzl0606@gmail.com>
2023-12-21 15:07:23 -0600

    Init commit

---
+++ components/FileUpload.js
@@ -0,0 +1,32 @@
+// components/FileUpload.js
+import React, { useState } from 'react';
+
+const FileUpload = () => {
+    const [file, setFile] = useState(null);
+
+    const handleFileChange = (e) => {
+        setFile(e.target.files[0]);
+    };
+
+    const handleUpload = async () => {
+        const formData = new FormData();
+        formData.append('file', file);
+
+        const response = await fetch('http://localhost:5000/upload', {
+            method: 'POST',
+            body: formData,
+        });
+
+        const data = await response.json();
+        console.log(data);
+    };
+
+    return (
+        <div>
+            <input type="file" onChange={handleFileChange} />
+            <button onClick={handleUpload}>Upload</button>
+        </div>
+    );
+};
+
+export default FileUpload;
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+// pages/index.js
+import FileUpload from '../components/FileUpload';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>File Upload</h1>
+            <FileUpload />
+        </div>
+    );
+};
+
+export default HomePage;
